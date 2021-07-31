befe71b9a1a6820015a254308083f73d8230f181
peternyangon <peternyangon@gmail.com>
2021-07-30 21:34:59 -0500

    my first lambda commit

--- src/LabdaPractices.kt
+++ src/LabdaPractices.kt
@@ -1,3 +1,19 @@
 fun main() {
     println("welcome to labda cladd")
+
+    val printMessage = {messege:String -> println(messege)}
+    printMessage("hey there")
+
+    val sum = {val1:Int,val2:Int ->
+        val total=val1+val2
+        total.toString()
+    }
+    println(sum(34,78))
+
+    val product={ x:Int, y:Int->
+        val product1=x+y
+        product1.toString()
+    }
+
+   println(product(67,89))
 }
